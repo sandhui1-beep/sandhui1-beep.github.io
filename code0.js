528b23c8cc7e19df25265ca9d5d2e7052d19bc56
sandhui1-beep <sandhui1@msu.edu>
2026-02-19 15:30:56 -0500

    fff

---
+++ code0.js
@@ -0,0 +1,21 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.localVariables = [];
+gdjs.Untitled_32sceneCode.idToCallbackMap = new Map();
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
